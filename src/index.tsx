6a3601122c70b88879275bb381682b537d511b0d
sergey-mikhovich <sergey.mikhovich98@gmail.com>
2025-11-04 12:35:53 +0300

    feat: add RTK Query baseApi and Redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import {createRoot} from "react-dom/client";
-import {App} from "@/app";
+import {App} from "@/app/app";
+import {Provider} from "react-redux";
+import {store} from "@/app/store";
 
-const root = document.getElementById('root');
+const container = document.getElementById('root');
 
-if (!root) {
-    throw new Error('root not found')
+if (container) {
+    const root = createRoot(container);
+
+    root.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    )
+} else {
+    throw new Error(
+        "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file."
+    )
 }
-
-const container = createRoot(root);
-
-container.render(<App/>)
