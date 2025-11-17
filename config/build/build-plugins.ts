f87c7a64cba0fda1504a23e4910a6e18ac23007c
sergey-mikhovich <sergey.mikhovich98@gmail.com>
2025-11-17 15:47:06 +0300

    ci: configure vercel deployment

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -1,4 +1,4 @@
-import {Configuration} from "webpack";
+import {Configuration, DefinePlugin} from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import Dotenv from "dotenv-webpack";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
@@ -8,14 +8,18 @@ export function buildPlugins({mode, paths}: BuildOptions): Configuration['plugin
     const isDev = mode === "development";
 
     const plugins: Configuration['plugins'] = [
-        new HtmlWebpackPlugin({template: paths.html}),
-        new Dotenv()
+        new HtmlWebpackPlugin({template: paths.html})
     ]
 
     if (isDev) {
         plugins.push(
+            new Dotenv(),
             new ReactRefreshWebpackPlugin(),
         )
+    } else {
+        plugins.push(new DefinePlugin({
+            'process.env.REACT_APP_BASE_URL': JSON.stringify(process.env.REACT_APP_BASE_URL)
+        }))
     }
 
     return plugins
--- src/core/services/base-api.ts
+++ src/core/services/base-api.ts
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.BASE_URL,
+        baseUrl: process.env.REACT_APP_BASE_URL,
         prepareHeaders: (headers) => {
             headers.set('Content-Type', 'application/json');
             headers.set('Authorization', `Bearer ${localStorage.getItem("accessToken")}`);
